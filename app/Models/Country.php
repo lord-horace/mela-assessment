bc2c3e1e59493e2259aea8365ebea0e2bf463ffd
lord-horace <pencilandchalk@gmail.com>
2025-08-06 08:58:10 +0200

    Add files via upload

---
+++ app/Models/Country.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Country extends Model
+{
+    /*
+    I could have done the following, but for the purposes of the assignment it was not needed
+
+    protected $table = 'countries'; // Optional, only needed if the table name is non-standard
+    protected $fillable = ['name', 'code']; // Optional, used for mass assignment protection
+
+    */
+}
